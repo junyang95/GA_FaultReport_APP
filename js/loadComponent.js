2dfb331a71f7aa6bafe1f1c6b51665680f821bfe
Sung Him Lui <sunghimlui@gmail.com>
2019-03-05 11:07:40 +0000

    moved inline javascript to loadComponent.js file

--- js/loadComponent.js
+++ js/loadComponent.js
@@ -1,5 +1,9 @@
 
-$(document).ready(function() {
-    $('#navbarComponent').load("../component/navbar.html");
 
+//Loading all the components here (all html files in component folder)
+//it uses AJAX to load (.load()) the external html files in
+$(document).ready(function () {
+    $('#navbarComponent').load("component/component/navbar.html"); //dk if component/component is a good name for it
+    $('#homeSectionComponent').load("component/page/homeSection.html");
+    $('#navFooter').load("component/component/navFooter.html");
 });
