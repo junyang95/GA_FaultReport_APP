ea058a09eee95faa7aa3a0a7c6b6aa0410dee96b
Sung Him Lui <sunghimlui@gmail.com>
2019-03-07 12:44:35 +0000

    completed validation for coach number,

--- js/faultReport.js
+++ js/faultReport.js
@@ -1,21 +1,59 @@
-function unhideCoach(){ //show the coach number
+//show the coach number
 
-    $("#coach").show();
-    $("#station").hide();
+
+function unhideCoach(){
+
+    //show coach
+    $("#unhideCoach").show();
+    $("#unhideStation").hide();
     $('#stationBox').css('background-color','lightgray');
     $('#coachBox').css('background-color','#D70428');
+
+    coaachNumberValidation(); //this function validate the coach number
+
 }
 
-function unhideStation(){ //show the station dropdown
+//show the station dropdown
 
-    $("#station").show();
-    $("#coach").hide();
+function unhideStation(){
+
+    //show station
+    $("#unhideStation").show();
+    $("#unhideCoach").hide();
     $('#stationBox').css('background-color','#D70428');
     $('#coachBox').css('background-color','lightgray');
 
     stationFilter();
 
+}
+//this function is to filter the station name
+
+function stationFilter(){
+
+    $("#stationInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#stationList li").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+
+    var stationInputValue = $('#stationInput').val();
+    var regex = /^[0-9]{4,6}$/;
+    var text = $("a li.selectedLi").val();
+
+    //when have time fix this here
+
+    if (!regex.test(stationInputValue) || !stationInputValue || !text) {
+
+        $('#issueTypeDropdown').hide();
+    }
+
     //this function allows to select station from dropdown
+
+    stationDropdownSelect();
+}
+
+function stationDropdownSelect(){
 
     $("#stationList").find("a li").click(function(){
         $('.selectedLi').removeClass('selectedLi');
@@ -25,42 +63,68 @@ function unhideStation(){ //show the station dropdown
         $('#stationInput').val(text);
         $('#stationList li').hide();
         $('#issueTypeDropdown').show();
+        displayIssueType("http://localhost:8081/getIssueType","issueTypeDropdown");
         //alert("selected Value:"+text);
 
     });
 
 }
 
-function stationFilter(){   //this function is to filter the station name
+function displayIssueType(path,disp_id){
 
-    $("#stationInput").on("keyup", function() {
-        var value = $(this).val().toLowerCase();
-        $("#stationList li").filter(function() {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-        });
+    $.ajax({
+        url: path,
+        type: "POST",
+        //data: json,
+        success: function(rt) {
+            console.log(rt); // returned data
+            var json = JSON.parse(rt); // the returned data will be an array
 
-        /*if($('#stationInput').val() == ""){
+            $('#'+disp_id).empty();
 
-            $('#issueTypeDropdown').hide();
-        }*/
+            $.each(json, function(i,val) {
 
+                console.log(val); //return first object
+                console.log(json); // returns all objects
 
+                var issueType = JSON.stringify(json);
 
+                //$('#'+disp_id).append(issueType + "<br/>");
+                $('#'+disp_id).append($('<option>', {
+                    value: issueType,
+                    text: issueType
+                }));
+            })
+        },
+        error: function(){
+            alert("error");
+        }
     });
-
-    var stationInputValue = $('#stationInput').val();
-    var regex = /^[0-9]{4,6}$/;
-
-    var text = $("a li.selectedLi").val();
-
-    //when have time fix this here
-
-    if (!regex.test(stationInputValue) || !stationInputValue || !text) {
-
-        $('#issueTypeDropdown').hide();
-    }
 }
 
+function coaachNumberValidation(){
+
+    $("#coachNumberInput").on("keyup", function() {
+
+        var coachNumberInputValue = $('#coachNumberInput').val();
+        //var regex = /\d\d\d\d\d\d/;
+        var regex = /\d{6}/;
+
+        if(!regex.test(coachNumberInputValue)||!coachNumberInputValue ){
+
+            $('#coachNumberInput').css('border','1px solid red');
+
+            $('#unhideSeatRequest').hide();
+
+        }else{
+            $('#coachNumberInput').css('border','1px solid lightgray');
+
+            $('#unhideSeatRequest').show();
+
+
+        }
+    });
+}
 
 
 
